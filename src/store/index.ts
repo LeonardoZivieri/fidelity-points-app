eaac7f6b1fac43846d4f6667c4939bdee9c59425
Leonardo Zivieri <leonardo.zivieri@misterprint.com.br>
2019-10-31 20:11:36 -0300

    Adicionado a classe base para uso do Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import State from './models/State'
+
+import AuthModule from './modules/auth'
+
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-	state: {
-	},
-	mutations: {
+export default new Vuex.Store<State>({
+	state: new State(),
+	modules: {
 	},
 	actions: {
+		init () {
+		},
 	},
-	modules: {
-	}
 })
