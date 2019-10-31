036f28e8525808426f25afca5acb2e75f77898e0
Leonardo Zivieri <leonardo.zivieri@misterprint.com.br>
2019-10-31 20:09:22 -0300

    Adicionado firebase

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import firebase from './firebase'
 import vuetify from './plugins/vuetify'
+
+firebase.initializeApp({
+	apiKey: 'AIzaSyAiya38K8feUb8fBOp9eVL0JnNYuA9nA-s',
+	authDomain: 'zivieri-app-unhas.firebaseapp.com',
+	databaseURL: 'https://zivieri-app-unhas.firebaseio.com',
+	projectId: 'zivieri-app-unhas',
+	storageBucket: 'zivieri-app-unhas.appspot.com',
+	messagingSenderId: '209188528919',
+	appId: '1:209188528919:web:e28cfe7f012efc7f358d52',
+})
 
 Vue.config.productionTip = false
 
@@ -11,5 +22,5 @@ new Vue({
 	router,
 	store,
 	vuetify,
-	render: h => h(App)
+	render: h => h(App),
 }).$mount('#app')
