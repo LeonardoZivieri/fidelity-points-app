00c1ee9e534589d3ab86e278f7907307640aeb37
Leonardo Zivieri <leonardo.zivieri@misterprint.com.br>
2019-10-30 22:20:05 -0300

    Changed Indent Style

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+	<v-app>
+		<v-app-bar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Vuetify</span>
+				<span class="font-weight-light">MATERIAL DESIGN</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn
+				text
+				href="https://github.com/vuetifyjs/vuetify/releases/latest"
+				target="_blank"
+			>
+				<span class="mr-2">Latest Release</span>
+			</v-btn>
+		</v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+		<v-content>
+			<HelloWorld/>
+		</v-content>
+	</v-app>
 </template>
 
 <script lang="ts">
@@ -26,12 +26,12 @@ import Vue from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
+	name: 'App',
+	components: {
+		HelloWorld
+	},
+	data: () => ({
+		//
+	})
 })
 </script>
