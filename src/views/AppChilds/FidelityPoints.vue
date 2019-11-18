cb54fef1c1d2f319f8148721ba14dca81c7845cb
Leonardo Zivieri <leonardo.zivieri@misterprint.com.br>
2019-11-18 20:53:56 -0300

    Colocado sistema de telas

--- src/views/AppChilds/FidelityPoints.vue
+++ src/views/AppChilds/FidelityPoints.vue
@@ -6,6 +6,109 @@
 					sm="8" offset-sm="2"
 					md="6" offset-md="3"
 				>
+				<v-row v-if="screen == 'loading'">
+					<v-col style="text-align: center">
+						<v-progress-circular
+								:size="50"
+								:width="4"
+								color="primary"
+								indeterminate
+							/>
+					</v-col>
+				</v-row>
+				<v-row v-if="screen == 'search'">
+					<v-col style="text-align: center">
+						<v-text-field
+								:loading="client === null && loading"
+								:disabled="client !== null && loading"
+								v-model="cpf"
+								label="CPF"
+								v-mask="'###.###.###-##'"
+								:append-icon="validCpf ? 'search' : 'warning'"
+								@click:append="search"
+								@keyup.enter="search"
+							/>
+					</v-col>
+				</v-row>
+				<v-row v-if="screen == 'edit'">
+					<v-col cols="12" class="py-1">
+						<h2 class="text-center">Cliente {{client.getDocumentFormatted()}}</h2>
+					</v-col>
+					<v-col
+							cols="12"
+							lg="6"
+						>
+						<v-text-field
+								:value="client.name"
+								@input="client.setName($event)"
+								label="Nome"
+								counter="100"
+							/>
+					</v-col>
+					<v-col
+							cols="12"
+							lg="6"
+						>
+						<v-text-field
+								v-model="dataNascimento"
+								label="Data Nascimento"
+								counter="100"
+								v-mask="'##/##/####'"
+							/>
+					</v-col>
+					<v-col cols="12">
+						<v-text-field
+								:value="client.email"
+								@input="client.setEmail($event)"
+								label="Email"
+								counter="100"
+							/>
+					</v-col>
+					<v-col cols="6">
+						<v-btn block color="secondary" @click="screen='search'">Cancelar</v-btn>
+					</v-col>
+					<v-col cols="6">
+						<v-btn block color="primary" @click="update">Salvar</v-btn>
+					</v-col>
+				</v-row>
+				<v-row v-if="screen == 'info'">
+					<v-col cols="12" class="py-1">
+						<h2 class="text-center">{{client.name}}</h2>
+					</v-col>
+					<v-col cols="12" class="py-1">
+						<h3 class="text-center">Documento {{client.getDocumentFormatted()}}</h3>
+					</v-col>
+					<v-col cols="12" class="py-1">
+						<v-btn block color="primary" @click="screen='edit'">Editar</v-btn>
+					</v-col>
+					<v-col cols="12" class="py-1">
+						<v-btn block color="accent" @click="screen='history'">Historico</v-btn>
+					</v-col>
+					<v-col cols="12" class="py-1">
+						<v-btn block color="info" @click="screen='registry'">Registrar Evento</v-btn>
+					</v-col>
+					<v-col cols="12" class="py-1">
+						<v-btn block color="secondary" @click="screen='search'">Procurar outro cliente</v-btn>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col cols="12" md="6" class="py-1">
+						<v-btn block color="secondary" @click="screen='info'">Voltar</v-btn>
+					</v-col>
+				</v-row>
+				<v-row v-if="screen == 'history'">
+					<v-col cols="12" class="py-1">
+						<v-btn block color="secondary" @click="screen='info'">Voltar</v-btn>
+					</v-col>
+				</v-row>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+					cols="12"
+					sm="8" offset-sm="2"
+					md="6" offset-md="3"
+				>
 				<v-text-field
 						:loading="client === null && loading"
 						:disabled="client !== null && loading"
@@ -85,6 +188,7 @@ export default {
 	name: 'AppIndex',
 	data () {
 		return {
+			screen: 'search',
 			cpf: '',
 			client: null,
 			loading: false,
@@ -139,14 +243,18 @@ export default {
 	methods: {
 		async search () {
 			if (!this.validCpf) return false
+			this.screen = 'loading'
 			this.loading = true
 			this.client = await this.$store.dispatch('customerConsult', this.cpf.replace(/[^0-9]/g, ''))
 			this.loading = false
+			this.screen = this.client.saved ? 'info' : 'edit'
 		},
 		async update () {
+			this.screen = 'loading'
 			this.loading = true
 			await this.$store.dispatch('customerSave', this.client)
 			this.loading = false
+			this.screen = 'info'
 		}
 	}
 }
