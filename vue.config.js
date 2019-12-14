8c579d93ab142bc5d140073d60e7e496db6f56f0
Leonardo Zivieri <leonardo.zivieri@misterprint.com.br>
2019-12-13 22:51:16 -0300

    Colocado plugin para PWA

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	'transpileDependencies': [
 		'vuetify'
-	]
+	],
+	'pwa': {
+		'name': 'App Fidelidade',
+		'themeColor': '#4DBA87',
+		'msTileColor': '#000000',
+		'appleMobileWebAppCapable': 'yes'
+	}
 }
